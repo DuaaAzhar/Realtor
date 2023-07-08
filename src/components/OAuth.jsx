014c23acdd31ac01bf6cb7ee0a0f731765d75c84
Duaa <duaaazhar03@gmail.com>
2023-07-09 00:19:04 +0500

    completed OAuth functionality( signUp with Google), check if user exists in DB or not

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -1,8 +1,40 @@
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
+import { doc, getDoc, serverTimestamp, setDoc } from 'firebase/firestore';
 import React from 'react'
 import {FcGoogle} from 'react-icons/fc'
+import { toast } from 'react-toastify';
+import { db } from '../firebase';
+import { useNavigate } from 'react-router';
+
+
 const OAuth = () => {
+  const navigate = useNavigate();
+  async function onGoogleClick(){
+    try {
+      const auth= getAuth();
+      const provider = new GoogleAuthProvider();
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+
+      //check if the user already exists in DB or not 
+      const docRef = doc(db, "users", user.uid);
+      const docSnap = await getDoc(docRef);
+
+      if(!docSnap.exists()){
+        await setDoc(docRef, {
+          name: user.displayName,
+          email: user.email,
+          timestamp: serverTimestamp()
+        });
+      }
+      navigate("/");
+
+    } catch (error) {
+      toast.error("Something went wrong with Google Authorization!")
+    }
+  }
   return (
-    <button className='flex items-center justify-center w-full
+    <button onClick={onGoogleClick} type="button" className='flex items-center justify-center w-full
     px-7 py-3 bg-red-500 text-white font-medium text-sm uppercase
     shadow-md hover:bg-red-700 active:bg-red-800 hover:shadow-lg 
     transition duration-150 ease-in-out rounded'>
